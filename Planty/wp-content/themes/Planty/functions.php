7eb5da696026ba696220edea21a9cc8e31c52909
Dylan TROUDE <dylantroude44@gmail.com>
2023-11-01 17:49:57 +0100

    commit1Planty

---
+++ Planty/wp-content/themes/Planty/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(),
+   filemtime(get_stylesheet_directory() . '/css/theme.css'));
+}
+function add_admin_button_to_menu($items, $args) {
+    if (is_user_logged_in() && $args->theme_location == 'primary') {
+        $admin_url = admin_url();
+        $admin_button = '<li id="admin-button" class="menu-item"><a href="' . $admin_url . '">Admin</a></li>';
+        $items = preg_replace('/<\/li>/', $admin_button . '</li>', $items, 1);
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'add_admin_button_to_menu', 10, 2);
